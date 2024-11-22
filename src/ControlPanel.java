5065213c0c613fa8061ef14ebca7de7072e9a01d
antariksh3225 <antarikshdev7@gmail.com>
2024-11-23 03:24:48 +0530

    better looking ui ig

--- src/ControlPanel.java
+++ src/ControlPanel.java
@@ -19,35 +19,49 @@ public class ControlPanel extends Component {
         mediator = screen;
         cp = new JPanel();
         cp.setLayout(null); // Set layout to null for absolute positioning
-        cp.setBackground(Color.LIGHT_GRAY);
+        cp.setBackground(Color.darkGray);
         currRotation = 0;
 
         JLabel l1 = new JLabel("ADD ROOM");
-        l1.setBounds(100, 0, 150, 30); // Set position and size
+        l1.setBounds(100, 0, 150, 30);
+        l1.setForeground(Color.WHITE);
+
+        l1.setFont(new Font("Arial",Font.BOLD,16));// Set position and size
         cp.add(l1);
 
         JButton r1 = new JButton("Bathroom");
         r1.setBounds(10, 40, 100, 30);
+        r1.setForeground(Color.WHITE);
+        r1.setBackground(Color.black);
         cp.add(r1);
 
         JButton r2 = new JButton("Living/Dining Room");
         r2.setBounds(170, 40, 100, 30);
+        r2.setForeground(Color.WHITE);
+        r2.setBackground(Color.black);
         cp.add(r2);
 
         JButton r3 = new JButton("Bedroom");
         r3.setBounds(10, 80, 100, 30);
+        r3.setForeground(Color.WHITE);
+        r3.setBackground(Color.black);
         cp.add(r3);
 
         JButton r4 = new JButton("Kitchen");
         r4.setBounds(170, 80, 100, 30);
+        r4.setForeground(Color.WHITE);
+        r4.setBackground(Color.black);
         cp.add(r4);
 
         JButton r5 = new JButton("Study Room");
         r5.setBounds(90,120, 100, 30);
+        r5.setForeground(Color.WHITE);
+        r5.setBackground(Color.black);
         cp.add(r5);
 
         JLabel k1JLabel = new JLabel("Position:");
         k1JLabel.setBounds(10, 140, 150, 30);
+        k1JLabel.setForeground(Color.WHITE);
         cp.add(k1JLabel);
 
         x.setBounds(30,170, 250, 30);
@@ -58,6 +72,7 @@ public class ControlPanel extends Component {
 
         JLabel k2JLabel = new JLabel("Dimensions:");
         k2JLabel.setBounds(10, 240, 150, 30);
+        k2JLabel.setForeground(Color.WHITE);
         cp.add(k2JLabel);
 
         h.setBounds(30, 265, 250, 30);
@@ -89,50 +104,75 @@ public class ControlPanel extends Component {
 
         JLabel l2 = new JLabel("ADD FURNITURE");
         l2.setBounds(100, 350, 100, 30);
+
+        l2.setForeground(Color.WHITE);
+        l2.setFont(new Font("Arial",Font.BOLD,12));
+
         cp.add(l2);
 
         JButton f1 = new JButton("Bed");
         f1.setBounds(10, 390, 100, 30);
+        f1.setForeground(Color.WHITE);
+        f1.setBackground(Color.black);
         cp.add(f1);
 
         JButton f2 = new JButton("Chair");
         f2.setBounds(170, 390, 100, 30);
+        f2.setForeground(Color.WHITE);
+        f2.setBackground(Color.black);
         cp.add(f2);
 
         JButton f3 = new JButton("Dining Set");
         f3.setBounds(10, 430, 100, 30);
+        f3.setForeground(Color.WHITE);
+        f3.setBackground(Color.black);
         cp.add(f3);
 
         JButton f4 = new JButton("Sofa");
         f4.setBounds(170, 430, 100, 30);
+        f4.setForeground(Color.WHITE);
+        f4.setBackground(Color.black);
         cp.add(f4);
 
         JButton f5 = new JButton("Table");
         f5.setBounds(10, 470, 100, 30);
+        f5.setForeground(Color.WHITE);
+        f5.setBackground(Color.black);
         cp.add(f5);
 
         JButton f6 = new JButton("Commode");
         f6.setBounds(170, 470, 100, 30);
+        f6.setForeground(Color.WHITE);
+        f6.setBackground(Color.black);
         cp.add(f6);
 
         JButton f7 = new JButton("Closet");
         f7.setBounds(10, 510, 100, 30);
+        f7.setForeground(Color.WHITE);
+        f7.setBackground(Color.black);
         cp.add(f7);
 
         JButton f8 = new JButton("Shower");
         f8.setBounds(170, 510, 100, 30);
+        f8.setForeground(Color.WHITE);
+        f8.setBackground(Color.black);
         cp.add(f8);
 
         JButton f9 = new JButton("Kitchen Sink");
         f9.setBounds(10, 550, 100, 30);
+        f9.setForeground(Color.WHITE);
+        f9.setBackground(Color.black);
         cp.add(f9);
 
         JButton f10 = new JButton("Stove");
         f10.setBounds(170, 550, 100, 30);
+        f10.setForeground(Color.WHITE);
+        f10.setBackground(Color.black);
         cp.add(f10);
 
         JLabel k3JLabel = new JLabel("Rotation:");
         k3JLabel.setBounds(10, 590, 150, 30);
+        k3JLabel.setForeground(Color.WHITE);
         cp.add(k3JLabel);
 
         r.setBounds(30, 620, 250, 30);
